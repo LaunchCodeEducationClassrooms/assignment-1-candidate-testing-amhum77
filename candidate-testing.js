4ffb41e0676c47487ee41d6706a7da74ac4d8241
ahumrich <amy.humrich@gmail.com>
2021-02-16 19:52:56 +0000

    Changed candidateAnswers converted to lower case in gradeQuiz instead of askQuestion.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = " ";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometers == 5000 meters? ","(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory","3"];
 let candidateAnswers= [];
 let grade;
@@ -23,10 +23,9 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (i=0; i< questions.length; i++){
   candidateAnswer = input.question(`${i+1}) ${questions[i]}`);
-    candidateAnswer = candidateAnswer.toLowerCase();
-    candidateAnswers.push(candidateAnswer);
+  candidateAnswers.push(candidateAnswer);
   console.log(`Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}`);
-  }
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -34,7 +33,7 @@ function gradeQuiz(candidateAnswers) {
   let grade;
   let points=0;
   for(i=0; i< candidateAnswers.length; i++){  
-    if (candidateAnswers[i] === correctAnswers[i].toLowerCase()){
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
         points = points + 1;
     }
   } 
