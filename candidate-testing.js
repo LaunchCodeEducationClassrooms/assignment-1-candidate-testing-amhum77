5a7cce5fa256b635241d7583965df1ea96adf47b
ahumrich <amy.humrich@gmail.com>
2021-02-16 19:05:47 +0000

    Created my personal solutions to CandidateTeseting

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,45 +3,58 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory","3"];
+let candidateAnswers= [];
+let grade;
+let status;
 
 function askForName() {
+  candidateName = input.question("What is your name?");
   // TODO 1.1b: Ask for candidate's name //
-
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  for (i=0; i< questions.length; i++){
+  candidateAnswer = input.question(`${i+1}) ${questions[i]}`);
+    candidateAnswer = candidateAnswer.toLowerCase();
+    candidateAnswers.push(candidateAnswer);
+  console.log(`Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}`);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
   let grade;
-  
-
+  let points=0;
+  for(i=0; i< candidateAnswers.length; i++){  
+    if (candidateAnswers[i] === correctAnswers[i].toLowerCase()){
+        points = points + 1;
+    }
+  } 
+  grade = (points/candidateAnswers.length)*100  
+  if (grade>= 80){
+    status = "Passed!";
+  } else {
+    status = "Google the answers and Try again";
+  }
+  console.log(`>>> Overall Grade: ${grade} (${points} of ${candidateAnswers.length} responses correct)<<<\n>>>> Status: ${status}<<<`);
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Greetings ${candidateName} welcome to your quiz`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
+  }
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
